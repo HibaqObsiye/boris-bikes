c341977f5a9a7e4aadca526b9cc20554bf5ba01f
Hibaq Obsiye <hibaqobsiye@Hibaqs-MacBook-Air.local>
2022-04-21 17:51:59 -0700

    bike respond to working

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -5,5 +5,9 @@ describe DockingStation do
     it 'responds to release_bike' do
         expect(subject).to respond_to(:release_bike)    
     end
+    it 'responds to working?' do
+      bike =  Bike.new
+      expect(bike).to respond_to(:working?)
+    end
 
 end
